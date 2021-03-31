b4ce4bf3de18da1c6467adc51146caec449a6a78
ben <me@benhunt.io>
2021-03-31 07:53:37 -0500

    Started Part2, Overview of HTML/DOM/CSS specs and the DOM in general

---
+++ src/js-tutorials/Part2/1.1-BrowserEnvSpecs.js
@@ -0,0 +1,15 @@
+/**
+ *  DOM - models a webpage as an object that you can edit. document is the entrypoint into editing this.
+ *
+ */
+/**
+ *  BOM - browser object model. Objects/methods provided by the host environment for working with everything BUT the document.
+ *  navigator.userAgent && navgiator.platform are some of the most common API's used.
+ */
+/**
+ * CSSOM - css object model - For styling.
+ */
+/**
+ * Host Environment - Each host environment provides it's own set of objects, or API. Browsers typically includes API's
+ * for manipulating the webpage.
+ */
---
+++ src/js-tutorials/Part2/1.2-DOMTree.js
@@ -0,0 +1,9 @@
+// Everything in HTML becomes a part of the DOM as a node.
+// 4 main node types
+/**
+ * Element Nodes: HTML tags. Making the tree structure
+ * Text Nodes: text between tags, always leafs
+ * Document Node: entry point node (root node)
+ * Comments Nodes: lolwut
+ * 
+ */
